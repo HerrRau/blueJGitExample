69d995a766f04607d282c181553637b880a9ad63
Herr Rau <github@herr-rau.de>
2020-04-04 10:07:36 +0200

    Initial sharing of project

---
+++ Kuh.java
@@ -0,0 +1,17 @@
+
+public class Kuh extends Tier
+{
+    public Kuh()
+    {
+    }
+
+    public String gibLaut() 
+    {
+        return "Muuuh.";
+    }
+
+    public String gibName() 
+    {
+        return "Kuh";
+    }
+}
---
+++ Tier.java
@@ -0,0 +1,10 @@
+public abstract class Tier
+{
+    public abstract String gibLaut();
+    public abstract String gibName();
+    
+    public final void wieMachtDasTier() 
+    {
+        System.out.println(gibName()+" macht "+gibLaut());
+    }
+}
