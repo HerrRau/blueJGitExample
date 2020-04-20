e1b041e26107f22a06ef4b5c48b0474341723c68
Rowena <rofrankb@gmail.com>
2020-04-20 18:00:36 +0200

    Fledermaus hinzugefügt.

---
+++ Fledermaus.java
@@ -0,0 +1,12 @@
+
+public class Fledermaus extends Tier {
+    
+    public Fledermaus() {}
+    
+    public String gibLaut() {
+        return "FlatterFlatter";}
+        
+    public String gibName() {
+        return "Fledermaus";}
+    }
+    
