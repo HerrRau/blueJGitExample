74b8ec0b7d47ab4a406ff0c7bd15041e80bcafc9
LilaLegolas <marnor.luan@gmx.net>
2020-04-20 15:55:09 +0200

    Mein tolles tier. Ein wahres Muss. Max

---
+++ Stirnlappenbasilisk.java
@@ -0,0 +1,17 @@
+
+public class Stirnlappenbasilisk extends Tier
+{
+    public Stirnlappenbasilisk()
+    {
+    }
+
+    public String gibLaut() 
+    {
+        return "neunLive";
+    }
+
+    public String gibName() 
+    {
+        return "Stirnlappenbasilisk";
+    }
+}
