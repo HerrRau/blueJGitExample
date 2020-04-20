8e1173c2b2ee75421523ed8398bdb7fa931d67d7
HerrRau <github@herr-rau.de>
2020-04-20 12:48:35 +0200

    Papagei hinzugefügt (Test)

---
+++ Papagei.java
@@ -0,0 +1,17 @@
+public class Papagei extends Tier
+{
+    public Papagei()
+    {
+    }
+
+    public String gibLaut() 
+    {
+        return "Krä-ächz";
+    }
+
+    public String gibName() 
+    {
+        return "Papagei";
+    }
+
+}
