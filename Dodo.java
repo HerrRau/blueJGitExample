810f5c53fdf375d705421fc4d2ae5908386388b1
TristanFelbermeier <warframe2xD@web.de>
2020-04-22 12:23:12 +0200

    Dodo hinzugefügt

---
+++ Dodo.java
@@ -0,0 +1,17 @@
+
+public class Dodo extends Tier
+{
+    public Dodo()
+    {
+    }
+
+    public String gibLaut() 
+    {
+        return "Gurr";
+    }
+
+    public String gibName() 
+    {
+        return "Dodo";
+    }
+}
