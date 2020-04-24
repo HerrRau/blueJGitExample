b95e0380b89c6a23ea36d428706ab56b93af5ed8
M4R4M0 <64258591+M4R4M0@users.noreply.github.com>
2020-04-24 15:27:08 +0200

    Einfügen der Uhu Klasse Übeltäter: M4R4M0

---
+++ Bubo_Bubo.java
@@ -0,0 +1,16 @@
+
+public class Bubo_Bubo extends Tier
+{
+   public Bubo_Bubo(){
+   }
+   public String gibLaut(){
+    return "whuuuh";
+    }
+   public String gibName(){
+     return "Bubo Bubo"; // Uhu wenn das jemanden interessiert
+    }
+    
+   
+
+
+}
