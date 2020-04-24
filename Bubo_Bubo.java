69a9907d61d537e1b6aa7c943f2b776add261e7f
M4R4M0 <64258591+M4R4M0@users.noreply.github.com>
2020-04-24 15:37:58 +0200

    Hinzufügen eines unnötigen Kommentars

--- Bubo_Bubo.java
+++ Bubo_Bubo.java
@@ -4,7 +4,7 @@ public class Bubo_Bubo extends Tier
    public Bubo_Bubo(){
    }
    public String gibLaut(){
-    return "whuuuh";
+    return "whuuuh"; // mir ist nichts besseres eingefallen
     }
    public String gibName(){
      return "Bubo Bubo"; // Uhu wenn das jemanden interessiert
