5cf5615ca541c9bf11203f5ca0cb65ae6d6c841e
Tim mit O <eee@gmail.com>
2022-05-05 15:04:22 +0200

    Das Axolotl ist da!

---
+++ Axolotl.java
@@ -0,0 +1,17 @@
+
+public class Axolotl extends Tier
+{
+    public Axolotl()
+    {
+    }
+
+    public String gibLaut() 
+    {
+        return "blub.";
+    }
+
+    public String gibName() 
+    {
+        return "Axolotl";
+    }
+}
