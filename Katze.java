8eb2fa3a2c2c0a7c49f5c0fbc8e7f7ad75645362
egal <egal@herr-rau.de>
2024-09-08 07:24:32 +0200

    Neue Katze

---
+++ Katze.java
@@ -0,0 +1,14 @@
+public class Katze extends Tier
+{
+    public Katze() {
+    }
+
+    public String gibLaut()  {
+        return "Mia.";
+    }
+
+    public String gibName() 
+    {
+        return "Katze";
+    }
+}
