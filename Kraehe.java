97a9724fb64c751df7bcbe048e7b7c3a78aea367
HerrRau <github@herr-rau.de>
2025-01-16 15:05:21 +0100

    Krähe

---
+++ Kraehe.java
@@ -0,0 +1,19 @@
+
+/**
+ * Beschreiben Sie hier die Klasse Kraehe.
+ * 
+ * @author (Ihr Name) 
+ * @version (eine Versionsnummer oder ein Datum)
+ */
+public class Kraehe extends Tier
+{
+    public String gibLaut() 
+    {
+        return "Kraaaechz";
+    }
+
+    public String gibName() 
+    {
+        return "Kraehe";
+    }
+}
