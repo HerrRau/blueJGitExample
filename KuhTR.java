006aca322e095ab968484a365d60c76c8741678f
frggghnmsf <schule@herr-rau.de>
2025-01-27 12:02:47 +0100

    Neue Kuh

--- KuhTR.java
+++ KuhTR.java
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-
 public class KuhTR 
 {
     String name;
@@ -35,55 +33,3 @@ public class KuhTR
     public String gibLaut() { return "Muh"; }
 
 }
-=======
-
-public class KuhTR 
-{
-    String name;
-    int groesse;
-    int gewicht;
-
-    public KuhTR()
-    {
-        name = "Fritz";
-        groesse = 5;
-        gewicht = 7;
-    }
-
-    public KuhTR(String naNeu, int grNeu, int geNeu)
-    {
-        name = naNeu;
-        groesse = grNeu;
-        gewicht = geNeu;
-    }
-
-    public void setzeName(String s) { 
-        name = s; 
-    }
-
-    public void setzeGroesse(int i) { 
-        groesse = i; 
-    }
-
-    public void setzeGewicht(int i) { 
-        gewicht = i; 
-    }
-
-    public String gibName() { 
-        return name; 
-    }
-
-    public int gibGroesse() { 
-        return groesse; 
-    } 
-
-    public int gibgewicht() { 
-        return gewicht; 
-    }
-
-    public String gibLaut() { 
-        return "Muh"; 
-    }
-
-}
->>>>>>> 4e7c6024570a077adbee94fc606f70c17dcc6d28
