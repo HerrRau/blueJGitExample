6e96682ad3cf9f918ca78cffa7dca4e939118357
nastya <shule.anastasiia@VGHGGH:DE>
2025-01-27 11:58:15 +0100

    MEIN TIER

---
+++ Katze.java
@@ -0,0 +1,36 @@
+public class Katze
+{
+    String name;
+    int groesse;
+    int gewicht;
+
+    public Katze()
+    {
+        name = "Mathilda";
+        groesse = 5;
+        gewicht = 7;
+    }
+
+    public Katze(String naNeu, int grNeu, int geNeu)
+    {
+        name = naNeu;
+        groesse = grNeu;
+        gewicht = geNeu;
+    }
+
+    public void setzeName(String s) { name = s; }
+
+    public void setzeGroesse(int i) { groesse = i; }
+
+    public void setzeGewicht(int i) { gewicht = i; }
+
+    public String gibName() { return name; }
+
+    public int gibGroesse() { return groesse; } 
+
+    public int gibgewicht() { return gewicht; }
+
+    public String gibLaut() { return "meow"; }
+
+}
+
