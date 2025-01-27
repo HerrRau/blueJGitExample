09c8cefd6c2ae993a14156286b964add53e1e29d
frggghnmsf <schule@herr-rau.de>
2025-01-27 12:02:04 +0100

    Hier ist meine Katze

---
+++ KatzeTR.java
@@ -0,0 +1,36 @@
+public class KatzeTR
+{
+    String name;
+    int groesse;
+    int gewicht;
+
+    public KatzeTR()
+    {
+        name = "Mathilda";
+        groesse = 5;
+        gewicht = 7;
+    }
+
+    public KatzeTR(String naNeu, int grNeu, int geNeu)
+    {
+        name = naNeu;
+        groesse = grNeu;
+        gewicht = geNeu;
+    }
+
+    public void setzeName(String s) { name = s; }
+
+    public void setzeGroesse(int i) { groesse = i; }
+
+    public void setzeGewicht(int i) { gewicht = i; }
+
+    public String gibName() { return name; }
+
+    public int gibGroesse() { return groesse; } 
+
+    public int gibgewicht() { return gewicht; }
+
+    public String gibLaut() { return "meow"; }
+
+}
+
