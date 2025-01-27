cadea3ae5ce3d77dc5bac8c3fe36ed1aca5dd0df
yana <er@fgh>
2025-01-27 11:59:36 +0100

    Mein Tier

---
+++ KatzeTR.java
@@ -0,0 +1,52 @@
+
+/**
+ * Beschreiben Sie hier die Klasse KatzeTR.
+ * 
+ * @author (Ihr Name) 
+ * @version (eine Versionsnummer oder ein Datum)
+ */
+public class KatzeTR
+{
+    // Attribute
+    String name; 
+    int groesse;
+    int gewicht;
+
+    /**
+     * Konstruktor für Objekte der Klasse KatzeTR
+     */
+    public KatzeTR()
+    { 
+        name = "Vinny";
+        groesse = 6; 
+        gewicht = 4;
+    }
+
+    // weitere Methoden
+     public KatzeTR(String naNeu, int grNeu, int geNeu)
+    {
+        name = naNeu;
+        groesse = grNeu;
+        gewicht = geNeu;
+    }
+
+    public void setzeName(String s) { name = s; }
+
+    public void setzeGroesse(int i) { groesse = i; }
+
+    public void setzeGewicht(int i) { gewicht = i; }
+
+    public String gibName() { return name; }
+
+    public int gibGroesse() { return groesse; } 
+
+    public int gibgewicht() { return gewicht; }
+
+    public String gibLaut() { return "Mau"; }
+
+}
+
+
+
+
+
